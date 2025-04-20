8ced8f42cbeb126105daaa5455e58020600b901e
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-04-20 09:04:30 +0300

    Add mini-game 'Guess number'

---
+++ mini-games.js
@@ -0,0 +1,50 @@
+// Описание игры для сайта («Угадай число»)
+/* Создайте игру, в которой пользователь пытается угадать случайное число от 1 до 100.
+Требования:
+Сгенерируйте случайное число от 1 до 100.
+Предложите пользователю угадать это число.
+Дайте подсказки: больше или меньше загаданное число.
+Завершите игру, когда пользователь угадает число.*/
+
+function guessNumber() {
+  // загадать целое число от 1 до 100
+  let number = Math.round(Math.random() * 100);
+  console.log(`Загаданное число: ${number}`);
+
+  // do {
+  while (true) {
+    // преобразовать введённое пользователем значение в число
+    let userAnswer = prompt("Угадайте число от 1 до 100\nВведите любое целое число от 1 до 100");
+    let userNumber = Number(userAnswer);
+
+    // если пользователь нажал "Отмена", то прервать игру
+    if (userAnswer === null) {
+      break;
+      // если введённое пользователем значение не число или не целое число от 1 до 100
+    } 
+    // else 
+    if (!isNaN(userNumber) || !(userNumber >= 1 && userNumber <= 100) || !(userNumber % 1 === 0)) {
+      // console.log(`\nЭто число, введённое пользователем: ${userNumber}, тип данных ${typeof (userNumber)}`);
+      // console.log(`\nЗначение выражения !isNaN(userNumber): ${!isNaN(userNumber)}`);
+      // console.log(`\nЗначение выражения !(userNumber < 1 && userNumber > 100): ${!(userNumber < 1 && userNumber > 100)}`);
+      // console.log(`\nЗначение выражения userNumber % 1 !== 0: ${userNumber % 1 !== 0}`);
+
+      alert('Вы ввели не число или не целое от 1 до 100');
+      // прервать текущую итерацию
+      continue;
+    }
+
+    // если введённое пользователем значение - целое число от 1 до 100
+    // console.log(`\nЭто userNumber после того, как польхователь ввёл целое число от 1 до 100: ${userNumber}, тип данных: ${typeof (userNumber)}`);
+    if (userNumber === number) {
+      alert(`Вы угадали! Загаданное число ${userNumber}`);
+      break;
+    } else if (number > userNumber) {
+      alert(`Загаданное число больше ${userNumber}`);
+      continue
+    } else if (number < userNumber) {
+      alert(`Загаданное число меньше ${userNumber}`);
+      continue
+    }
+  }
+}
