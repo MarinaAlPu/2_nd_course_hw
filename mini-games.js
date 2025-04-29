1e79038bc55105d91a5fb1ea95249c62a9433a79
MarinaAlPu <marina.pudovkina13@gmail.com>
2025-04-29 21:17:33 +0300

    Fix card-number in about-games. Add mini-game 2

--- mini-games.js
+++ mini-games.js
@@ -20,8 +20,8 @@ function guessNumber() {
     if (userAnswer === null) {
       break;
       // если введённое пользователем значение не число или не целое число от 1 до 100
-    } 
- 
+    }
+
     if (isNaN(userNumber) || (userNumber < 1) || (userNumber > 100) || (userNumber % 1 !== 0)) {
       // console.log(`\nЭто число, введённое пользователем: ${userNumber}, тип данных ${typeof (userNumber)}`);
       // console.log(`\nЗначение выражения isNaN(userNumber): ${isNaN(userNumber)}`);
@@ -46,6 +46,109 @@ function guessNumber() {
     } else if (number < userNumber) {
       alert(`Загаданное число меньше ${userNumber}`);
       continue
+    }
+  }
+}
+
+
+// Описание игры для сайта («Простая арифметика»)
+/* Создайте игру, где пользователю предлагаются простые арифметические задачи
+Требования:
+Сайт генерирует случайные задачи на сложение, вычитание, умножение и деление.
+Запрашивает у пользователя ответ.
+Проверяет правильность ответа и выводит результат.
+Последовательность действий
+ Генерация задач:
+Случайным образом создавайте арифметические задачи на сложение, вычитание, умножение и деление.
+Например, задачи могут выглядеть так: "5 + 3", "10 - 2", "4 * 7", "20 / 4".
+ Запрос ответа:
+Запрашивайте у пользователя ответ на сгенерированную задачу.
+Например, используя функцию prompt().
+ Проверка и вывод результата:
+Проверьте правильность ответа пользователя.
+Выведите результат проверки: верный ответ или ошибка.*/
+
+function simpleArithmetic() {
+  let mathOperations = ['+', '-', '*', '/']
+
+  function randomNumber() {
+    let number = Math.round(Math.random() * 9) + 1;
+    return number;
+  }
+
+  while (true) {
+    let number1 = randomNumber();
+    let number2 = randomNumber();
+    let answer;
+    let userAnswer = "";
+
+    let indexForMathOperation = Math.round(Math.random() * 3);
+    // console.log(indexForMathOperation);
+
+    let operation = mathOperations[indexForMathOperation];
+    // console.log(operation);
+
+    // let task = String(number1) + " " + operation + " " + String(number2);
+    let task = String(3) + "-" + String(3);
+
+
+    switch (indexForMathOperation) {
+      case 0:
+        answer = number1 + number2;
+        // console.log(number1 + number2);
+        break;
+      case 1:
+        answer = number1 - number2;
+        // console.log(number1 - number2);
+        break;
+      case 2:
+        answer = number1 * number2;
+        // console.log(number1 * number2);
+        break;
+      case 3:
+        answer = (number1 / number2).toFixed(2);
+        // console.log(number1 / number2);
+        break;
+    }
+    answer = 0;
+    console.log("\nЭто answer:");
+    console.log(answer);
+
+    let userAnswerForChecks = (prompt(`Решите задачу:\n${task} =\nОтвет впишите в поле ниже`)).trim();
+    // let userAnswer = Number(prompt(`Решите задачу:\n${task} =\nОтвет впишите в поле ниже`));
+    // console.log(userAnswer);
+
+    if (userAnswerForChecks === null) {
+      break;
+    }
+
+    console.log("\nЭто ответ пользователя до проверки");
+    console.log(userAnswerForChecks);
+    console.log(typeof(userAnswerForChecks));
+
+    if (userAnswerForChecks.includes(',')) {
+      userAnswerForChecks = userAnswerForChecks.replace(',', '.');
+      // console.log("\nЭто ответ пользователя в проверке");
+      // console.log(userAnswerForChecks);
+    }
+
+    // console.log("\nЭто ответ пользователя после проверки");
+    // console.log(userAnswerForChecks);
+
+    if (indexForMathOperation === 3) {
+      userAnswer = Number(userAnswerForChecks).toFixed(2);
+      // console.log("\nЭто ответ пользователя после округления");
+      // console.log(userAnswer);
+    } else {
+      userAnswer = Number("\nЭто ответ пользователя в Number");
+      userAnswer = Number(userAnswerForChecks);
+    }
+
+    if (userAnswer === answer || userAnswerForChecks !== "" && userAnswerForChecks === '0' && typeof(userAnswerForChecks) === string && answer === 0) {
+    // if (userAnswer === answer) {
+      alert("Ваш ответ верный!")
+    } else {
+      alert("Ошибка.")
     }
   }
 }
